ec4218fc90dbd450277ad27c167cc42385ce60c9
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-10-24 20:12:46 -0400

    feat: first commit

---
+++ exemplo-pacotes/src/lanchonete/cozinheiro.java
@@ -0,0 +1,14 @@
+package lanchonete;
+
+public class cozinheiro {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
